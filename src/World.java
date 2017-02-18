3ffba1f01104a2da41a89f9e15c999e231ebb8b0
Jordan Buckmaster <buckmast@purdue.edu>
2017-02-18 16:22:49 -0500

    looping around edges

--- src/World.java
+++ src/World.java
@@ -3,85 +3,101 @@ import java.util.List;
 
 public class World 
 {
-	
+
 	private List<Player> playerList;
 	private List<Block> blockList;
-	
+
+	private double width;
+	private double height;
 	public final double ACCELERATION = -9.8;
-	
-	public World()
+
+	public World(double width, double height)
 	{
+		this.width = width;
+		this.height = height;
 		playerList = new ArrayList<Player>();
 		blockList = new ArrayList<Block>();	
 	}
-	
+
 	public void update(double delta)
 	{
 		for(Player player : playerList) {
 			//block collisions, y-axis
 			player.updateYMotion();
-			for(Block block : blockList) {
-				if(isColliding(player, block)){
-					if(player.getYVel() > 0) {
-						player.setYPos(block.getYPos() - player.getHeight());
-						player.land();
-					} else {
-						player.setYPos(block.getYPos()+block.getHeight());
-						player.setYVel(0);
-					}
-				}
-			}
-			//player collisions, y-axis
-			for(Player otherPlayer : playerList) {
-				if(otherPlayer.getCharacter() != player.getCharacter()) 
-				{
-					if(isColliding(player, otherPlayer)){
+			if(player.getYPos() >= height) {
+				player.setYPos(player.getHeight());
+			} else if(player.getYPos() <= 0) {
+				player.setYPos(height);
+			} else {
+				for(Block block : blockList) {
+					if(isColliding(player, block)){
 						if(player.getYVel() > 0) {
-							player.setYPos(otherPlayer.getYPos() - player.getHeight());
+							player.setYPos(block.getYPos() - player.getHeight());
 							player.land();
 						} else {
-							player.setYPos(otherPlayer.getYPos()+otherPlayer.getHeight());
+							player.setYPos(block.getYPos()+block.getHeight());
 							player.setYVel(0);
 						}
 					}
 				}
-			}
-			
-			//block collisions, x-axis
-			player.updateXMotion();
-			for(Block block : blockList) {
-				if(isColliding(player, block)) {
-					if(player.getXVel() > 0) {
-						player.setXPos(block.getXPos()-player.getWidth());
-						player.setXVel(0);
-					} else {
-						player.setXPos(block.getXPos()+block.getWidth());
-						player.setXVel(0);
+				//player collisions, y-axis
+				for(Player otherPlayer : playerList) {
+					if(otherPlayer.getCharacter() != player.getCharacter()) 
+					{
+						if(isColliding(player, otherPlayer)){
+							if(player.getYVel() > 0) {
+								player.setYPos(otherPlayer.getYPos() - player.getHeight());
+								player.land();
+							} else {
+								player.setYPos(otherPlayer.getYPos()+otherPlayer.getHeight());
+								player.setYVel(0);
+							}
+						}
 					}
 				}
 			}
-			//player collisions, x-axis
-			for(Player otherPlayer : playerList) {
-				if(otherPlayer.getCharacter() != player.getCharacter())
-				if(isColliding(player, otherPlayer)) {
-					if(player.getXVel() > 0) {
-						player.setXPos(otherPlayer.getXPos()-player.getWidth());
-						player.setXVel(0);
-					} else {
-						player.setXPos(otherPlayer.getXPos()+otherPlayer.getWidth());
-						player.setXVel(0);
+
+			//block collisions, x-axis
+			player.updateXMotion();
+			if(player.getXPos() >= width) {
+				player.setXPos(0-player.getWidth());
+			} else if (player.getXPos() <= 0) {
+				player.setXPos(width);
+			} else {
+				for(Block block : blockList) {
+					if(isColliding(player, block)) {
+						if(player.getXVel() > 0) {
+							player.setXPos(block.getXPos()-player.getWidth());
+							player.setXVel(0);
+						} else {
+							player.setXPos(block.getXPos()+block.getWidth());
+							player.setXVel(0);
+						}
 					}
+				}
+				//player collisions, x-axis
+				for(Player otherPlayer : playerList) {
+					if(otherPlayer.getCharacter() != player.getCharacter())
+						if(isColliding(player, otherPlayer)) {
+							if(player.getXVel() > 0) {
+								player.setXPos(otherPlayer.getXPos()-player.getWidth());
+								player.setXVel(0);
+							} else {
+								player.setXPos(otherPlayer.getXPos()+otherPlayer.getWidth());
+								player.setXVel(0);
+							}
+						}
 				}
 			}
 		}
-		
+
 	}
-	
+
 	public void render(double delta)
 	{
-		
+
 	}
-	
+
 	public List<Player> getPlayers(){
 		return playerList;
 	}
@@ -108,28 +124,28 @@ public class World
 		playerList.add(player);
 		return true;
 	}
-	
+
 	public void updateVelocityAcceleration(Player player){
-			double yVel = player.getYVel();
-			double xVel = player.getXVel();
-			double yAcc = player.getYAcc();
-			double xAcc = player.getXAcc();
-			double oldX = player.getXPos();
-			double oldY = player.getYPos();
-			
-			player.setYVel(yVel + yAcc);
-			player.setXVel(xVel + xAcc);
-			player.setX(oldX + xVel);
-			player.setY(oldY + yVel);
+		double yVel = player.getYVel();
+		double xVel = player.getXVel();
+		double yAcc = player.getYAcc();
+		double xAcc = player.getXAcc();
+		double oldX = player.getXPos();
+		double oldY = player.getYPos();
+
+		player.setYVel(yVel + yAcc);
+		player.setXVel(xVel + xAcc);
+		player.setX(oldX + xVel);
+		player.setY(oldY + yVel);
 	}
-	
+
 	public boolean isColliding(Block p1, Block p2) {
 		return (p1.getXPos() < p2.getXPos() + p2.getWidth() &&
-				   p1.getXPos() + p1.getWidth() > p2.getXPos() &&
-				   p1.getYPos() < p2.getYPos() + p2.getHeight() &&
-				   p1.getHeight() + p1.getYPos() > p2.getYPos());
+				p1.getXPos() + p1.getWidth() > p2.getXPos() &&
+				p1.getYPos() < p2.getYPos() + p2.getHeight() &&
+				p1.getHeight() + p1.getYPos() > p2.getYPos());
 	}
-	
+
 	public boolean isLowerBoundColliding(Block p1, Block p2) {
 		return !(p1.getHeight() + p1.getYPos() > p2.getYPos());
 	}
