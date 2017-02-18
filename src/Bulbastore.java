4c2bd7594d0ed39061683c5968bf6cf10e48f602
Jordan Buckmaster <buckmast@purdue.edu>
2017-02-18 18:35:43 -0500

    Bulbastore explosion ability

--- src/Bulbastore.java
+++ src/Bulbastore.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import com.polaris.engine.util.MathHelper;
 
 public class Bulbastore extends Player {
 	
@@ -23,10 +27,51 @@ public class Bulbastore extends Player {
 	}
 	
 	public void explode() {
-		for(double i = 0; i < (Math.PI)*2; i += Math.PI/180) {
+		for(double i = 0; i < (Math.PI)*2; i += Math.PI/18) {
+			double slope = Math.tan(i);
+			double midX = this.getXPos() + (this.getWidth())/2;
+			double midY = this.getYPos() + (this.getHeight())/2;
+			double radius = 175 + damageCounter/3;
+			double distance = 0;
+			double xIncr = 10;
+			double x = 10;
+			double y = slope*x+midY;
 			
+			Block collidedWith = lineIsColliding(x, y);
+			
+			while(collidedWith == null && distance <= radius) {
+				distance = MathHelper.pythagoreon(x-midX, y-midY);
+				x += xIncr;
+				y = slope*x+midY;
+				collidedWith = lineIsColliding(x, y);
+			}
+			if(collidedWith instanceof Player) {
+				this.damage(damageCounter/3, (Player)collidedWith);
+			}
 		}
 		
+	}
+	
+	public Block lineIsColliding(double x, double y) {
+		List<Player> playersList = GuiWorld.world.getPlayers();
+		List<Block> blockList = GuiWorld.world.getBlocks();
+		for(Player player : playersList) {
+			if(player.getCharacter() != CharacterType.STORE) {
+				if(x > player.getXPos() && x < player.getXPos() + 
+					player.getWidth() && y > player.getYPos() &&
+					y < player.getYPos() + player.getHeight()) {
+					return player;
+				}
+			}
+		}	
+		for(Block block : blockList) {
+			if(x > block.getXPos() && x < block.getXPos() + 
+					block.getWidth() && y > block.getYPos() &&
+					y < block.getYPos() + block.getHeight()) {
+					return block;
+				}
+		}
+		return null;
 	}
 	
 	public void render(double delta) {
