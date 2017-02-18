65df5d02ecebb404afc21d411443e1cf6da41b22
Jordan Buckmaster <buckmast@purdue.edu>
2017-02-18 13:11:54 -0500

    modified character classes further

--- src/Jumpernaut.java
+++ src/Jumpernaut.java
@@ -1,7 +1,13 @@
 
 public class Jumpernaut extends Player {
 
+	
 	public Jumpernaut(double x, double y) {
 		super(x, y);
+		characterType = "JUMP";
+	}
+	
+	public void slam() {
+		//Jumpernaut has a special slam, overrides slam in player
 	}
 }
--- src/Loadstar.java
+++ src/Loadstar.java
@@ -3,5 +3,6 @@ public class Loadstar extends Player {
 
 	public Loadstar(double x, double y) {
 		super(x, y);
+		characterType = "LOAD";
 	}
 }
