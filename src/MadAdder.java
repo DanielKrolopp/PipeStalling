cc65c8ef8222b9925daf61a5f21790462c63950f
Daniel Krolopp <dkrolopp@purdue.edu>
2017-02-18 16:45:53 -0500

    Updated MadAdder.java

--- src/MadAdder.java
+++ src/MadAdder.java
@@ -1,21 +1,19 @@
 
 public class MadAdder extends Player {
 	
-	private final int specialDamage = 2; //Can be modified
+	private final int SPECIAL_DAMAGE = 2; //Can be modified
 	
 	public MadAdder(double x, double y) {
 		super(x, y);
 		characterType = CharacterType.ADD;
 	}
 	
-	
-	
 	public void special() {
 		usingSpecial = true;
 		double centerY = (2* getYPos() + size)/2;
 		double centerX = (2 * getXPos() + size)/2;
 		double minDistance = Double.MAX_VALUE;
-		Player closestVictim;
+		Player closestVictim = null;
 		if(facingLeft){
 			for(Player player : GuiWorld.world.getPlayers()){
 				double other_top = player.getYPos();
@@ -49,6 +47,13 @@ public class MadAdder extends Player {
 					}
 				}
 			}
+		}
+		if(closestVictim != null){ //Means he hit someone
+			this.damage(SPECIAL_DAMAGE, closestVictim);
+			this.heal(SPECIAL_DAMAGE);
+		}
+		else{ //Indicates a miss
+			this.damage(SPECIAL_DAMAGE, this);
 		}
 	}
 	
