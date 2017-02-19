f54c3e1a6bc51e56c3486cc53a4536080d3e39d0
Killian-LeClainche <kleclain@purdue.edu>
2017-02-18 22:12:09 -0500

    All over the place with this one

--- src/CharacterType.java
+++ src/CharacterType.java
@@ -1,3 +1,17 @@
+import org.joml.Vector3d;
+
 public enum CharacterType {
-	JUMP, ADD, LOAD, STORE;
+	JUMP(new Vector3d(0, 191 / 255d, 1d)), ADD(new Vector3d(128 / 255d, 0, 0)), LOAD(new Vector3d(204 / 255d, 204 / 255d, 0)), STORE(new Vector3d(50 / 255d, 205 / 255d, 50 / 255d));
+
+	private Vector3d color;
+	
+	private CharacterType(Vector3d vec)
+	{
+		color = vec;
+	}
+	
+	public Vector3d getColor()
+	{
+		return color;
+	}
 }
