6a42bb82bb49320eeb101a37badd5bacd2d6fa4e
JordanR159 <reed226@purdue.edu>
2017-02-18 22:03:24 -0600

    fixes

--- src/World.java
+++ src/World.java
@@ -31,9 +31,9 @@ public class World
 		assignTypes(playerCharacters);
 		if(numPlayers == 2)
 			spawnTwo();
-		if(numPlayers == 2)
+		if(numPlayers == 3)
 			spawnThree();
-		if(numPlayers == 2)
+		if(numPlayers == 4)
 			spawnFour();
 	}
 	
